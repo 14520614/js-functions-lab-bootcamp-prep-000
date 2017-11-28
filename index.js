61ad4b353b1658a562b80b7c69cc52ee883ab3d0
Trịnh Bảo Khai Nguyên <14520614@gm.uit.edu.vn>
2017-11-28 13:48:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // write your code below!
+function happyHolidays(string)
+{
+  return "Happy holidays!"
+}
+var name = "Nguyen"
+var holiday = "sinh nhat cu ho"
+function happyHolidaysTo(name)
+{
+  return "Happy holidays, ${name}!"
+}
+function happyHolidaysTo(holiday, name)
+{
+  return " Happy ${holiday}, ${name}!"
+}
